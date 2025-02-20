75b4d2942b313f74e859f6d102833a7525eea38f
mburakow <mburakow@vbox.mburakow.virtualbox.org>
2025-02-20 17:09:48 +0200

    working

--- scrs/requirements/wordpress/tools/entrypoint.sh
+++ scrs/requirements/wordpress/tools/entrypoint.sh
@@ -1,32 +1,12 @@
 #!/bin/sh
 
+# Wait for MariaDB 
 until nc -z "$WP_DB_HOST" 3306; do
     echo "Waiting for MariaDB..."
     sleep 5
 done
 
-# # Check if wp-config.php exists, create if missing
-# if [ ! -f "/var/www/html/wp-config.php" ]; then
-#     echo "Creating wp-config.php..."
-#     cat << EOF > /var/www/html/wp-config.php
-# 		<?php
-# 		define('DB_NAME', '${WP_DB_NAME}');
-# 		define('DB_USER', '${WP_DB_USER}');
-# 		define('DB_PASSWORD', '${WP_DB_PASSWORD}');
-# 		define('DB_HOST', '${WP_DB_HOST}');
-# 		define('DB_CHARSET', 'utf8');
-# 		define('DB_COLLATE', '');
-# 		\$table_prefix = 'wp_';
-# 		define('WP_DEBUG', false);
-# 		if ( ! defined('ABSPATH') ) {
-# 			define('ABSPATH', dirname(__FILE__) . '/');
-# 		}
-# 		require_once(ABSPATH . 'wp-settings.php');
-# EOF
-# fi
-
-
-# Check if wp-config.php exists, create if missing
+# Check if wp-config.php exists, create if missing and populate values
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     echo "Creating wp-config.php from template..."
     cp /usr/local/bin/wp-config.php /var/www/html/wp-config.php
@@ -53,23 +33,24 @@ if ! wp core is-installed --allow-root; then
     # Install WordPress
     wp core install --allow-root \
         --url="https://${DOMAIN_NAME}" \
-        --title="My WordPress Site" \
+        --title="${WP_TITLE}" \
         --admin_user="${WP_ADMIN}" \
         --admin_password="${WP_ADMIN_PSW}" \
         --admin_email="${WP_ADMIN_EMAIL}" \
         --skip-email
-
-    # Configure site settings
-    wp option update siteurl "https://${DOMAIN_NAME}" --allow-root
-    wp option update home "https://${DOMAIN_NAME}" --allow-root
-
-	echo "Domain name is ${DOMAIN_NAME}" 
 
 	wp theme install twentytwentythree --activate --allow-root
 
     echo "WordPress installation completed!"
 else
     echo "WordPress is already installed!"
+fi
+
+# Get the favicon! This is important!
+if ! wp option get site_icon --allow-root | grep -q '^[1-9][0-9]*$'; then
+    echo "Importing favicon and setting as site icon..."
+    ATTACHMENT_ID=$(wp media import ./favicon.ico --porcelain --allow-root)
+    wp option update site_icon $ATTACHMENT_ID --allow-root
 fi
 
 # Ensure ownership and permissions
